1825b65a62f95f1ca5e7aabb1e279d69cb8cc40f
Janielle Joy Gregorio <hello_janielle1992@yahoo.com.ph>
2020-05-14 20:00:25 +0800

    MySQL Connector

---
+++ src/testcases/TestConfig.java
@@ -0,0 +1,32 @@
+package testcases;
+public class TestConfig{
+
+
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "arrowphoto2112259janiellejoy@gmail.com";
+	public static String password = "YouAreAmazing987";
+	public static String[] to ={"selenium_test@gmail.com", "hello_test@yahoo.com.ph"};
+	public static String subject = "Extent Project Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath="C:\\Users\\hello\\Pictures\\Evolution of Red Blood Cells.jpg";
+	public static String attachmentName="error.jpg";
+	
+	
+	
+	//SQL DATABASE DETAILS	
+	public static String driver="net.sourceforge.jtds.jdbc.Driver"; 
+	public static String dbConnectionUrl="jdbc:jtds:sqlserver://192.101.44.22;DatabaseName=monitor_eval"; 
+	public static String dbUserName="sa"; 
+	public static String dbPassword="$ql$!!1"; 
+	
+	
+	//MYSQL DATABASE DETAILS
+	public static String mysqldriver="com.mysql.cj.jdbc.Driver";
+	public static String mysqluserName = "root";
+	public static String mysqlpassword = "@MySQLJan92";
+	public static String mysqlurl = "jdbc:mysql://localhost:3306/13thmay2020";
+
+	
+}
